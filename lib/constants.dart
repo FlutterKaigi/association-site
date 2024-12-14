3260d3444b8d4e5b7d350050912f5763d031512d
jiyuujin <jiyuujinunite@gmail.com>
2024-12-14 12:26:27 +0900

    initialize

---
+++ lib/constants.dart
@@ -0,0 +1,30 @@
+import 'package:associate_site/models/info_model.dart';
+
+const title = '一般社団法人FlutterKaigi / FlutterKaigi Association';
+
+final List<Info> info = [
+  Info(label: '名称', text: title),
+  Info(label: '主たる事務所', text: '東京都渋谷区渋谷2-19-15宮益坂ビルディング609'),
+  Info(label: '法人の公告方法', text: '当法人の公告は、電子公告により行う'),
+  Info(
+      label: '目的等',
+      text: '目的 当法人は、エンジニアの技術共有およびコミュニケーションを支援、充実することを目的とし、その目的に資するため、次の事業を行う'),
+];
+
+final notices = [
+  [
+    '2023年度(令和5年度)貸借対照表',
+    'https://drive.google.com/file/d/178O_RqmSR-qdaSGbv9RN6IoEuM7I6If8/view?usp=drive_link'
+  ],
+  [
+    '2024年度(令和6年度)貸借対照表',
+    'https://drive.google.com/file/d/1bNfLuma7ZMzeX_wOzu5GCvfG8RMASXXD/view?usp=drive_link'
+  ],
+];
+
+final purposes = [
+  'エンジニアを対象とした催しの開催、また運営',
+  'エンジニアの育成の推進を図る活動',
+  '技術共有、コミュニケーションを目的とした催しの支援および人材育成',
+  '前各号に附帯又は関連する事業',
+];
---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:associate_site/components/title_section.dart';
+import 'package:associate_site/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text(title),
+      ),
+      body: Container(
+        color: Colors.deepPurple[50],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('assets/flutterkaigi_icon.png'),
+              ),
+              ...info.map((i) {
+                return TitleSection(
+                  name: i.label,
+                  children: [
+                    Text(i.text, style: TextStyle(color: Colors.grey[500])),
+                    (i.label == '法人の公告方法' || i.label == '目的等')
+                        ? const Gap(32)
+                        : const SizedBox(),
+                    ...i.label == '法人の公告方法'
+                        ? notices
+                            .map(
+                              (n) => Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        n[0],
+                                        style: TextStyle(
+                                          color: Colors.grey[500],
+                                        ),
+                                      ),
+                                      InkWell(
+                                        splashColor: Colors.transparent,
+                                        onTap: () async {
+                                          Uri url = Uri.parse(n[1]);
+                                          await launchUrl(url);
+                                        },
+                                        child: Container(
+                                          height: 30,
+                                          width: 100,
+                                          color: Colors.red,
+                                          child: const Center(
+                                            child: Text(
+                                              'PDF を開く',
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const Gap(8),
+                                ],
+                              ),
+                            )
+                            .toList()
+                        : [const SizedBox()],
+                    ...i.label == '目的等'
+                        ? purposes
+                            .map(
+                              (p) => Text(
+                                p,
+                                style: TextStyle(color: Colors.grey[500]),
+                              ),
+                            )
+                            .toList()
+                        : [const SizedBox()],
+                  ],
+                );
+              }).toList(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
