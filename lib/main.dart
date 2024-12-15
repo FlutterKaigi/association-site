c7eff90d831be3ee7fecc7c2b676fbae62a6f807
jiyuujin <jiyuujinunite@gmail.com>
2024-12-15 09:31:13 +0900

    fix: scroll page

--- lib/main.dart
+++ lib/main.dart
@@ -34,82 +34,85 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text(title),
       ),
-      body: Container(
-        color: Colors.deepPurple[50],
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('assets/flutterkaigi_icon.png'),
-              ),
-              ...info.map((i) {
-                return TitleSection(
-                  name: i.label,
-                  children: [
-                    Text(i.text, style: TextStyle(color: Colors.grey[500])),
-                    (i.label == '法人の公告方法' || i.label == '目的等')
-                        ? const Gap(32)
-                        : const SizedBox(),
-                    ...i.label == '法人の公告方法'
-                        ? notices
-                            .map(
-                              (n) => Column(
-                                children: [
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        n[0],
-                                        style: TextStyle(
-                                          color: Colors.grey[500],
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.deepPurple[50],
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Gap(32),
+                const CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('assets/flutterkaigi_icon.png'),
+                ),
+                ...info.map((i) {
+                  return TitleSection(
+                    name: i.label,
+                    children: [
+                      Text(i.text, style: TextStyle(color: Colors.grey[500])),
+                      (i.label == '法人の公告方法' || i.label == '目的等')
+                          ? const Gap(32)
+                          : const SizedBox(),
+                      ...i.label == '法人の公告方法'
+                          ? notices
+                              .map(
+                                (n) => Column(
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          n[0],
+                                          style: TextStyle(
+                                            color: Colors.grey[500],
+                                          ),
                                         ),
-                                      ),
-                                      InkWell(
-                                        splashColor: Colors.transparent,
-                                        onTap: () async {
-                                          Uri url = Uri.parse(n[1]);
-                                          await launchUrl(url);
-                                        },
-                                        child: Container(
-                                          height: 30,
-                                          width: 100,
-                                          color: Colors.red,
-                                          child: const Center(
-                                            child: Text(
-                                              'PDF を開く',
-                                              style: TextStyle(
-                                                color: Colors.white,
-                                                fontWeight: FontWeight.bold,
+                                        InkWell(
+                                          splashColor: Colors.transparent,
+                                          onTap: () async {
+                                            Uri url = Uri.parse(n[1]);
+                                            await launchUrl(url);
+                                          },
+                                          child: Container(
+                                            height: 30,
+                                            width: 100,
+                                            color: Colors.red,
+                                            child: const Center(
+                                              child: Text(
+                                                'PDF を開く',
+                                                style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
                                               ),
                                             ),
                                           ),
                                         ),
-                                      ),
-                                    ],
-                                  ),
-                                  const Gap(8),
-                                ],
-                              ),
-                            )
-                            .toList()
-                        : [const SizedBox()],
-                    ...i.label == '目的等'
-                        ? purposes
-                            .map(
-                              (p) => Text(
-                                p,
-                                style: TextStyle(color: Colors.grey[500]),
-                              ),
-                            )
-                            .toList()
-                        : [const SizedBox()],
-                  ],
-                );
-              }).toList(),
-            ],
+                                      ],
+                                    ),
+                                    const Gap(8),
+                                  ],
+                                ),
+                              )
+                              .toList()
+                          : [const SizedBox()],
+                      ...i.label == '目的等'
+                          ? purposes
+                              .map(
+                                (p) => Text(
+                                  p,
+                                  style: TextStyle(color: Colors.grey[500]),
+                                ),
+                              )
+                              .toList()
+                          : [const SizedBox()],
+                    ],
+                  );
+                }).toList(),
+              ],
+            ),
           ),
         ),
       ),
